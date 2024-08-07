a53656145014b0f82a3f307f67d93fdfdea5887a
RITHVIK1101 <rithviksaba@gmail.com>
2024-08-07 10:49:43 -0700

    new

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import React, { useEffect, useState } from 'react';
+import moment from 'moment';
+import './App.css'; // Import the CSS file
+
+function App() {
+  const [timeLeft, setTimeLeft] = useState({
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  });
+
+  useEffect(() => {
+    let endDate = localStorage.getItem('countdownEndDate');
+    
+    if (!endDate) {
+      endDate = moment().add(70, 'days').add(8, 'hours').add(5, 'minutes').add(20, 'seconds').valueOf();
+      localStorage.setItem('countdownEndDate', endDate);
+    }
+
+    const targetDate = moment(parseInt(endDate));
+
+    const calculateTimeLeft = () => {
+      const now = moment();
+      const duration = moment.duration(targetDate.diff(now));
+
+      return {
+        days: Math.floor(duration.asDays()),
+        hours: duration.hours(),
+        minutes: duration.minutes(),
+        seconds: duration.seconds(),
+      };
+    };
+
+    const updateTimer = () => {
+      const newTimeLeft = calculateTimeLeft();
+      setTimeLeft(newTimeLeft);
+    };
+
+    updateTimer();
+    const timerInterval = setInterval(updateTimer, 1000);
+
+    return () => clearInterval(timerInterval);
+  }, []);
+
+  return (
+    <div className="container">
+      <div className="countdown-container">
+        <div className="timer-box">
+          <div className="timer">
+            {`${timeLeft.days}D ${timeLeft.hours}H ${timeLeft.minutes}M ${timeLeft.seconds}S`}
+          </div>
+          <a href="https://tally.so/r/3lL6Ro" target="_blank" rel="noopener noreferrer" className="access-link">
+            Get early access →
+          </a>
+        </div>
+      </div>
+      <h1 className="main-title">
+        The future of AI.<br />
+        <span className="subtitle">Find, create, deploy, and embed<br/> </span>
+        <span className='new'>AI models</span>
+      </h1>
+    </div>
+  );
+}
+
+export default App;
